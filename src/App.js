aaf20cf373b117cee4d24c35b50e530b2a9006ce
Gathrean Dela Cruz <gathrean@icloud.com>
2023-05-31 13:39:46 -0700

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './fonts/Benzin Bold.ttf';
+import './styles/App.css';
 
-function App() {
+function Navbar() {
+  // Use state to track the active page
+  const [activePage, setActivePage] = useState('home');
+
+  // Function to handle click event and update active page
+  const handlePageClick = (page) => {
+    setActivePage(page);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar benzin-font">
+      <img className="navbar-logo" src="ean star text.png" alt="Ean Star Text" />
+
+      <ul className="navbar-links">
+        <li>
+          <a
+            href="#"
+            className={activePage === 'home' ? 'active' : ''}
+            onClick={() => handlePageClick('home')}
+          >
+            home{activePage === 'home' ? '✦' : ''}
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className={activePage === 'discography' ? 'active' : ''}
+            onClick={() => handlePageClick('discography')}
+          >
+            discography{activePage === 'discography' ? '✦' : ''}
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className={activePage === 'beats' ? 'active' : ''}
+            onClick={() => handlePageClick('beats')}
+          >
+            beats{activePage === 'beats' ? '✦' : ''}
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className={activePage === 'contact' ? 'active' : ''}
+            onClick={() => handlePageClick('contact')}
+          >
+            contact{activePage === 'contact' ? '✦' : ''}
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
-export default App;
+export default Navbar;
