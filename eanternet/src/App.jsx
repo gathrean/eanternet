7b505501ec99ccd41e2be05662a58d5c18d411d1
Gathrean Dela Cruz <gathrean@icloud.com>
2025-08-29 22:34:30 -0700

    Set up Eanternet with Vite + GitHub Pages

---
+++ eanternet/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/App.css';
+import './styles/index.css';
+import './styles/fonts.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="title">EANTERNET</h1>
+    </div>
+  );
+}
+
+export default App;
